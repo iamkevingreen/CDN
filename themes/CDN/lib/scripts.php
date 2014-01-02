37a16ec5d6c90acd1efa4f4693d6447a6e93086e
Kevin Green <thedudem4n@gmail.com>
2014-01-01 19:33:54 -0500

    more style tweaks

--- themes/CDN/lib/scripts.php
+++ themes/CDN/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '14fdd828b4fba14dcec00f3625d68e04');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f331c68fa8eba59bfca2631793b46d24');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
