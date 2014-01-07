bd2d0b51c6e615a17218890e41f37cb5e4c322e1
Kevin Green <thedudem4n@gmail.com>
2014-01-06 23:29:43 -0500

    opacity hover on footer links

--- themes/CDN/lib/scripts.php
+++ themes/CDN/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '01e6dbff88a89a09098e86548b99565b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '392665b9c00dca71d2a14891281a4620');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
