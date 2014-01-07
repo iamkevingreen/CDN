e19bc3bc33a5193b8f931230a52ba6f529c78dd0
Kevin Green <thedudem4n@gmail.com>
2014-01-06 21:06:53 -0500

    remove hover transition

--- themes/CDN/lib/scripts.php
+++ themes/CDN/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cca7a4702bfb6a3b63e9b602fc1a0f61');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ed257ba0b1f8f49d1fe00614837a9879');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
