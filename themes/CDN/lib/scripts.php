a9bc99da57ae94558dfa4edfcd2409eed397365b
Kevin Green <thedudem4n@gmail.com>
2014-03-24 22:59:48 -0400

    updated some styles added a fly cursor

--- themes/CDN/lib/scripts.php
+++ themes/CDN/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cede15efad5ee73de0ae7e0334d80790');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3d57d944a345f953fe9bd28dfe200c63');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
